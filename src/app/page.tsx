b72a5901a484e6785eafd07ee7cf8990640ac9c0
daniyalahmee <daniyalahmee11@gmail.com>
2024-01-04 13:08:16 +0500

    update reademe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,126 @@
+
+
+// import React, { useEffect, useState } from 'react';
+
+// ... rest of your component code
+
 import Image from 'next/image'
+
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="h-full w-full bg-white">
+        <header className="h-29 w-full  bg-gray-50 flex-grow ">
+          <div className="flex items-center justify-center space-x-3">
+            <p className=" font-extrabold text-4xl text-green-800 font-serif text-center mb-2 mt-2 ml-2">
+              Green Soil
+            </p>
+            <Image src={"/logo.png"} alt="logo" width={60} height={60} />
+          </div>
+        </header>
+        <header className=" flex justify-end w-full bg-gray-50 h-9 items-center space-x-3 p-4  ">
+          <button className="text-white rounded-2xl px-3 text-center justify-center h-7 font-serif     bg-green-800">
+            Sign up
+          </button>
+          {/* <button className="text-white rounded-2xl px-3 text-center justify-center h-7 font-serif     bg-green-800">
+            About Us
+          </button> */}
+        </header>
+        <div className="flex justify-center w-full h-6 bg-green-800 mt-1  rounded-lg px-4 mx-auto">
+          <p className="animate-slideLeft text-white font-serif font-light">
+            Organic Waste to Organic Ferlizer
+          </p>
         </div>
+        <div className='mt-[20px]'>
+          <div className="grid grid-flow-row     mb-3 p-3  grid-cols-2 space-y-4">
+            <div className="text-6xl text-green-800 font-serif bg-gray-50 text-center rounded-l-md items-center justify-center ">
+              4
+            </div>
+            <div className="bg-green-800 text-white font-serif text-justify p-4 rounded-r-md ">
+              2 Lorem, Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Quo est quidem sunt. Corrupti maiores id voluptatibus natus cumque
+              quibusdam quos doloribus, hic excepturi reprehenderit, voluptatum
+              recusandae quae ea incidunt optio!ipsum dolor sit amet consectetur
+              adipisicing elit. Voluptatem molestias iure dolorem id neque
+              provident blanditiis amet odio reprehenderit repudiandae
+              quibusdam, vel obcaecati, quo odit dignissimos quos reiciendis,
+              quaerat vitae?
+            </div>
+            <div className="bg-green-800 text-white font-serif text-justify p-4 rounded-r-md">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+              blanditiis possimus nostrum, ratione dolorem deleniti fuga ducimus
+              quae et sit necessitatibus, excepturi aperiam asperiores? Et nam
+              nemo eos quibusdam atque. Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Deleniti voluptas error quos tenetur nam ea
+              suscipit excepturi quam, nemo ex quisquam tempora quia ipsa
+              molestias laborum impedit non eum dignissimos.
+            </div>
+            <div className="text-6xl text-green-800 font-serif bg-gray-50 text-center rounded-l-md items-center justify-center ">
+              4
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-row md:flex-auto justify-between mt-3 p-3  rounded-lg  items-center mx-auto ">
+          <p className=" text-justify  font-serif   ">
+            Step into a world where every seed planted is a promise of life,
+            where soil isn't just dirt but a canvas for nature's masterpiece. At
+            Green Soil, we're on a mission to redefine your gardening
+            experience. Our organic fertilizer, crafted from the heart of
+            sustainability, breathes life into your garden, creating a sanctuary
+            of vibrant colors and thriving greenery. Join us in cultivating not
+            just gardens, but a greener, healthier planet. Discover the essence
+            of Green Soil where growth meets grace, and your garden becomes a
+            testament to the beauty of nature
+          </p>
+          <button className=" text-white font-medium rounded-full m-3  flex justify-end bg-green-800 px-3 mx-auto">
+            Shop Now
+          </button>
+        </div>
+        <></>
+
+        <footer className="bg-green-800  flex justify-evenly h-auto absolute w-full text-white">
+          <div className=" justify-start font-serif text-center">
+            <p className="text-lg font-semibold m-3 text-gray-50">ABOUT US</p>
+            <p>About</p>
+            <p>Terms Of Service</p>
+            <p>Our Mission</p>
+          </div>
+          <div className=" justify-start font-serif text-center">
+            <p className="text-lg font-semibold m-3 text-gray-50">
+              CUSTOMER SERVICE
+            </p>
+            <p>Terms & conditions</p>
+            <p>Privacy policy</p>
+            <p>FAQ's</p>
+          </div>
+          <div className=" justify-start font-serif text-center">
+            <p className="text-lg font-semibold m-3 text-gray-50">CONTACT US</p>
+            <p>Call Us At: +92302 0268181</p>
+            <p>Email Us: support@greensoil.com.pk</p>
+          </div>
+          <div className=" justify-start font-serif text-center">
+            <p className="text-lg font-semibold m-3 text-gray-50">
+              SIGN UP FOR UPDATES
+            </p>
+            <div>
+              <input
+                type="Enter Your Email"
+                className="border-black rounded-l-lg bg-green-900"
+              />
+              <button className="rounded-r-lg bg-white text-green-800 px-4 font-serif">
+                SUBSCRIBE
+              </button>
+            </div>
+          </div>
+        </footer>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
+
+
+
+
